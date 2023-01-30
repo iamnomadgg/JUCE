bdfa744918c47dcbd30617cb0276d02d0d8b7f47
iamnomadgg <gokhangocmen94@gmail.com>
2023-01-30 11:54:54 +0100

    Customized Toggle Button graphics

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -152,12 +152,17 @@ private:
 
 struct LookAndFeel : juce::LookAndFeel_V4
 {
-    void drawRotarySlider(juce::Graphics&,
+    void drawRotarySlider(juce::Graphics& g,
         int x, int y, int width, int height,
         float sliderPosProportional,
         float rotaryStartAngle,
         float rotaryEndAngle,
         juce::Slider&) override;
+    
+    void drawToggleButton(juce::Graphics& g,
+        juce::ToggleButton& toggleButton,
+        bool shouldDrawButtonAsHighlighted,
+        bool shouldDrawButtonAsDown) override;
 };
 
 struct RotarySliderWithLabels : juce::Slider
@@ -292,6 +297,8 @@ private:
         analyzerEnabledButtonAttachment;
 
     std::vector<juce::Component*> getComps();
+
+    LookAndFeel lnf;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
